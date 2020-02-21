a863ecac96dbc3f1aedf34ac6fe371241868bfbb
vera1994 <767760879@qq.com>
2020-02-21 09:16:21 +0800

    初始化

---
+++ beetlsql-service/src/test/java/com/lianxun/beetlsqlservice/BeetlsqlServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lianxun.beetlsqlservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BeetlsqlServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
